c59dd6a94ffe2320211059f688cf0a75a8ac7b13
KCLKellyKC <diwater0120@gmail.com>
2026-07-07 14:56:32 +0800

    大改版

--- config.js
+++ config.js
@@ -1,1 +1,5 @@
-window.MWS_CONFIG={SUPABASE_URL:"https://dqsxliknvtqxqkyuhdag.supabase.co",SUPABASE_PUBLISHABLE_KEY:"sb_publishable_SjIXZHhq6eaz84A3yu8PnA_inrNs527",REDIRECT_TO:"https://kclkellykc.github.io/wishing-sky/"};
+window.MWS_CONFIG = {
+  SUPABASE_URL: "https://dqsxliknvtqxqkyuhdag.supabase.co",
+  SUPABASE_ANON_KEY: "sb_publishable_SjIXZHhq6eaz84A3yu8PnA_inrNs527",
+  REDIRECT_TO: window.location.origin + window.location.pathname
+};
