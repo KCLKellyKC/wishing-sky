4881951bd02e06baf594655a211ddfe32509df6b
KCLKellyKC <diwater0120@gmail.com>
2026-07-08 11:20:43 +0800

    Update config.example.js

--- config.example.js
+++ config.example.js
@@ -1,1 +1,6 @@
-window.MWS_CONFIG={SUPABASE_URL:'https://YOUR_PROJECT_ID.supabase.co',SUPABASE_ANON_KEY:'YOUR_PUBLISHABLE_KEY',SUPABASE_PUBLISHABLE_KEY:'YOUR_PUBLISHABLE_KEY',REDIRECT_TO:window.location.origin+window.location.pathname};
+window.MWS_CONFIG = {
+  SUPABASE_URL: "https://dqsxliknvtqxqkyuhdag.supabase.co",
+  SUPABASE_ANON_KEY: "sb_publishable_SjIXZHhq6eaz84A3yu8PnA_inrNs527",
+  SUPABASE_PUBLISHABLE_KEY: "sb_publishable_SjIXZHhq6eaz84A3yu8PnA_inrNs527",
+  REDIRECT_TO: window.location.origin + window.location.pathname
+};
