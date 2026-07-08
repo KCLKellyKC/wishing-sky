dd91212686352498a4c1d3077836020cf3a2bb4a
KCLKellyKC <diwater0120@gmail.com>
2026-07-08 11:53:19 +0800

    Delete config.example.js

--- config.example.js
+++
@@ -1,6 +0,0 @@
-window.MWS_CONFIG = {
-  SUPABASE_URL: "https://dqsxliknvtqxqkyuhdag.supabase.co",
-  SUPABASE_ANON_KEY: "sb_publishable_SjIXZHhq6eaz84A3yu8PnA_inrNs527",
-  SUPABASE_PUBLISHABLE_KEY: "sb_publishable_SjIXZHhq6eaz84A3yu8PnA_inrNs527",
-  REDIRECT_TO: window.location.origin + window.location.pathname
-};
